970e23ce0aeed155a39606bd360defcfae78c19c
Paul J. Warner <pjwarn@gmail.com>
2025-03-30 19:54:11 -0500

    This is the first commit of working code.

---
+++ notebooks/tie_breakers.py
@@ -0,0 +1,16 @@
+tie_breakers = [
+	'number of pitches thrown by starting pitchers combined.',
+	'strikeouts thrown by Orioles relievers.',
+	'number of walks taken by Orioles batters',
+	'total number of Orioles hits.',
+	'total relievers used by both teams.',
+	'combined number of hits by both teams.',
+    'Total IP (ex 5.2) of both SP combined.',
+    'Total combined XBH.',
+    'Total number of hits both teams combined.',
+    'Total balls thrown by Starter/Openers.',
+    'Total strikeouts thrown by Orioles STARTERS',
+    'Total strikeouts thrown by both starters combined',
+    'Total number of minutes the game will run'
+]
+	
